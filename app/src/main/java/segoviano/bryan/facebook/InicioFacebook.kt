c90b8af75999cf5779773b1e0ffb88c75af75122
Bryan <bryanisaac77@hotmail.com>
2022-02-10 20:16:37 -0700

    Facebook App Moviles

---
+++ app/src/main/java/segoviano/bryan/facebook/InicioFacebook.kt
@@ -0,0 +1,11 @@
+package segoviano.bryan.facebook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InicioFacebook : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inicio_facebook)
+    }
+}
